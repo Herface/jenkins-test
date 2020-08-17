9749d38ba5b451364fa5c22296cc176e923a3e83
holyhit <asdfghjklhcy@hotmail.com>
2020-08-17 17:35:57 +0800

    首次提交

---
+++ src/main/java/com/yong/jenkinstest/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.yong.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Hes
+ */
+@RestController
+public class IndexController {
+
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+}
