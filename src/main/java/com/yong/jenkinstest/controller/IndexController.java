481816c5222832416807789e05b7fe84fbb2d7ff
holyhit <asdfghjklhcy@hotmail.com>
2020-08-17 17:52:13 +0800

    新增Dockerfile

--- src/main/java/com/yong/jenkinstest/controller/IndexController.java
+++ src/main/java/com/yong/jenkinstest/controller/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("index")
     public String index(){
         return "index";
     }
